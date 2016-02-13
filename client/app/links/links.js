68f6d76eb64d04bb843a66ab6c9ae5b40837b324
jgliner <jarrett.gliner@gmail.com>
2016-02-12 17:30:30 -0800

    Invoke controller getAll

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getAll = function () {
+    Links.getAll()
+    .then(function (data) {
+      $scope.data.links = data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  $scope.getAll();
 });
