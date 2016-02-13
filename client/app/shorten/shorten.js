77bcf676c0c798a773694eb495109d2e26b8fe1d
jgliner <jarrett.gliner@gmail.com>
2016-02-12 18:53:26 -0800

    Implement controller for addLink in shorten.js

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,15 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  $scope.link = {};
+
+  $scope.addLink = function (link) {
+    Links.addOne(link)
+    .then(function (data) {
+      $scope.link = data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
 });
